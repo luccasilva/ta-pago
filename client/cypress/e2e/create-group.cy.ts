61384dad7c9e105e599bbc9f0496a667bf6cefcc
CaioSilvaDti <caio.silva@dtidigital.com.br>
2023-06-18 17:35:47 -0300

    feat: adding group creation

---
+++ client/cypress/e2e/create-group.cy.ts
@@ -0,0 +1,39 @@
+describe("CreateGroup Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.get('input[name="email"]').type("novo_caio@gmail.com");
+    cy.get('input[name="password"]').type("Teste@1234");
+    cy.get("form").submit();
+    cy.get("button").contains("+").click();
+  });
+
+  it("should create a new group", () => {
+    cy.intercept("POST", "/api/groups").as("createGroup");
+
+    cy.get("input[name='name']").type("New Group Name");
+    cy.get("input[name='description']").type("New Group Description");
+
+    cy.get("button").contains("Criar").click();
+
+    cy.contains("Grupo criado com sucesso!").should("exist");
+  });
+
+  it("should join a group", () => {
+    cy.intercept("POST", "/api/groups").as("createGroup");
+
+    cy.get("input[name='name']").type("New Group Name");
+    cy.get("input[name='description']").type("New Group Description");
+
+    cy.get("button").contains("Criar").click();
+
+    cy.get("input[name='group-code']")
+      .invoke("val")
+      .then((value) => {
+        const groupValue = value; // Cast the value to a string
+        cy.get("input[name='tag']").type(groupValue);
+        cy.get("button").contains("Entrar").click();
+      });
+
+    cy.contains("Você entrou no grupo!").should("exist");
+  });
+});
