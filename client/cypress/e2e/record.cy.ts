23012aa7758ae932ee84e166bc5e5d3af16bd433
CaioSilvaDti <caio.silva@dtidigital.com.br>
2023-06-18 17:14:07 -0300

    feat: adding record tests

---
+++ client/cypress/e2e/record.cy.ts
@@ -0,0 +1,33 @@
+describe("Record Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.get('input[name="email"]').type("novo_caio@gmail.com");
+    cy.get('input[name="password"]').type("Teste@1234");
+    cy.get("form").submit();
+    cy.get("a[href='/record']").click();
+  });
+
+  it("registers a new record", () => {
+    cy.intercept("POST", "/api/record").as("registerRecord");
+
+    cy.get("input[name='name']").type("New Record Name");
+    cy.get("input[name='description']").type("New Record Description");
+
+    cy.get("button").contains("Cadastrar").click();
+
+    cy.contains("Ficha cadastrada com sucesso!").should("exist");
+  });
+
+  it("deletes a record", () => {
+    cy.intercept("DELETE", "/api/record").as("deleteRecord");
+
+    cy.get("input[name='name']").type("New Record Name");
+    cy.get("input[name='description']").type("New Record Description");
+
+    cy.get("button").contains("Cadastrar").click();
+
+    cy.get("button").contains("Deletar").first().click();
+
+    cy.contains("Ficha removida com sucesso!").should("exist");
+  });
+});
