2e7cf0724b5f1ba110d3c9e324caedee59f32da4
CaioSilvaDti <caio.silva@dtidigital.com.br>
2023-06-18 15:06:48 -0300

    feat: adding more tests

---
+++ client/cypress/e2e/login.cy.ts
@@ -0,0 +1,35 @@
+// login.spec.ts
+/// <reference types="Cypress" />
+
+describe("Login Component", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000"); // Assuming the login component is rendered at the "/login" route
+  });
+
+  it("should display the login form", () => {
+    cy.get("form").should("exist");
+    cy.get('input[name="email"]').should("exist");
+    cy.get('input[name="password"]').should("exist");
+    cy.contains("Logar").should("exist");
+  });
+
+  it("should show an error message for invalid credentials", () => {
+    cy.get('input[name="email"]').type("invalid@example.com");
+    cy.get('input[name="password"]').type("wrongpassword");
+    cy.get("form").submit();
+    cy.contains("Error occurred during login.").should("exist");
+  });
+
+  it("should redirect to home page after successful login", () => {
+    cy.get('input[name="email"]').type("valid@example.com");
+    cy.get('input[name="password"]').type("password123");
+    cy.get("form").submit();
+    cy.url().should("include", "/home");
+    cy.contains("Logado com sucesso!").should("exist");
+  });
+
+  it("should navigate to the registration page when 'Cadastre-se' button is clicked", () => {
+    cy.contains("Cadastre-se").click();
+    cy.url().should("include", "/register");
+  });
+});
